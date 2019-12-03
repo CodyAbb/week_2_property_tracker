4fd3c704ef602870b559b4e6f7293728c1a8d708
Cody Abbott <cody.abbott16@gmail.com>
2019-12-03 15:07:18 +0000

    created console file with pry binding

---
+++ console.rb
@@ -0,0 +1,19 @@
+require('pry')
+require_relative('models/property_tracker')
+
+property1 = PropertyInfo.new({
+  'address' => "7 Castle Terrace",
+  'value' => 10000,
+  'number_of_bedrooms' => 15,
+  'build' => "commercial"
+  })
+
+property2 = PropertyInfo.new({
+  'address' => "1 Princes Street",
+  'value' => 2000000,
+  'number_of_bedrooms' => 4,
+  'build' => "mixed-use"
+  })
+
+binding.pry
+nil
